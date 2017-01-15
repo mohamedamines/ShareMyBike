76b797103dc57dc7165913739b3a769e0c5c4972
azerty <azerty@MacBook-Pro-de-azerty.local>
2017-01-15 16:53:33 +0100

    simplified service

--- src/main/scala/ShareMyBikeService.scala
+++ src/main/scala/ShareMyBikeService.scala
@@ -1,26 +1,70 @@
 import akka.actor.ActorSystem
-import akka.event.Logging
+import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import akka.Done
+import akka.event.Logging
+import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import com.typesafe.config.ConfigFactory
-import spray.json.DefaultJsonProtocol
+import spray.json.DefaultJsonProtocol._
 
-case class Bike(weight: Option[Double], speed: Option[Double], description: Option[String])
+import scala.io.StdIn
+import scala.concurrent.Future
 
+object ShareMyBikeService {
 
-trait Protocols extends DefaultJsonProtocol {
+  // domain model
+  case class Bike(id: Long, speed: Double, description: String)
 
-  implicit val bikeFormat = jsonFormat3(Bike.apply)
+  // formats for unmarshalling and marshalling
+  implicit val bikeFormat = jsonFormat3(Bike)
+
+  // fake async database query api
+  def fetchBike(id: Long): Future[Option[Bike]] = ???
+  def saveBike(bike: Bike): Future[Done] = ???
+
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem()
+    implicit val executor = system.dispatcher
+    implicit val materializer = ActorMaterializer()
+
+    val config = ConfigFactory.load()
+    val logger = Logging(system, getClass)
+
+    val route: Route =
+      get {
+        pathPrefix("bike" / LongNumber) { id =>
+          val maybeBike: Future[Option[Bike]] = fetchBike(id)
+
+          onSuccess(maybeBike) {
+            case Some(item) => complete(item)
+            case None => complete(StatusCodes.NotFound)
+          }
+        }
+      } ~
+    post {
+      path("create-bike") {
+        entity(as[Bike]) { bike =>
+          val saved: Future[Done] = saveBike(bike)
+          onComplete(saved) { done =>
+            complete("bike saved")
+
+          }
+        }
+      }
+
+    }
+
+    val bindingFuture = Http().bindAndHandle(route, config.getString("http.interface"), config.getInt("http.port"))
+
+    println(s"Server online at http://localhost:9000/\nPress RETURN to stop...")
+    StdIn.readLine()
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
 
 }
-
-object ShareMyBikeService extends App {
-
-  implicit val system = ActorSystem()
-  implicit val executor = system.dispatcher
-  implicit val materializer = ActorMaterializer()
-
-  val config = ConfigFactory.load()
-  val logger = Logging(system, getClass)
-
-}
-
