73febdbe0657b1b9e87bb1f0da5ffe180f1aa3f2
azerty <azerty@MacBook-Pro-de-azerty.local>
2017-01-22 20:16:26 +0100

    fixed issue

--- src/main/scala/Bikes.scala
+++
@@ -1,8 +0,0 @@
-import scala.slick.driver.PostgresDriver.simple._
-
-  class Bikes(tag: Tag) extends Table[(Int, Int, String)](tag, "bikes") {
-    def id = column[Int]("id")
-    def speed = column[Int]("speed")
-    def description = column[String]("description")
-    def * = (id, speed, description)
-  }
--- src/main/scala/ShareMyBikeService.scala
+++ src/main/scala/ShareMyBikeService.scala
@@ -11,14 +11,29 @@ import com.typesafe.config.ConfigFactory
 import spray.json.DefaultJsonProtocol._
 
 import scala.io.StdIn
-import scala.concurrent.{ExecutionContext, Future}
 import scala.slick.lifted.TableQuery
 import scala.slick.driver.PostgresDriver.simple._
 
 
-class ShareMyBikeService(implicit val executionContext: ExecutionContext) {
+object ShareMyBikeService {
+
+    class Bikes(tag: Tag) extends Table[(Int, Int, String)](tag, "bikes") {
+      def id = column[Int]("id")
+      def speed = column[Int]("speed")
+      def description = column[String]("description")
+      def * = (id, speed, description)
+    }
+
 
   def main(args: Array[String]): Unit = {
+
+    implicit val system = ActorSystem()
+    implicit val executor = system.dispatcher
+    implicit val materializer = ActorMaterializer()
+
+
+    val config = ConfigFactory.load()
+    val logger = Logging(system, getClass)
 
     val connectionUrl = "jdbc:postgresql://localhost:5433/med?user=postgres&password=medamine"
 
@@ -27,65 +42,57 @@ class ShareMyBikeService(implicit val executionContext: ExecutionContext) {
     implicit val bikeFormat = jsonFormat3(Bike)
 
     Database.forURL(connectionUrl, driver = "org.postgresql.Driver") withSession {
-    implicit session =>
-      val bikes = TableQuery[Bikes]
+      implicit session =>
 
-      val bikesList: List[Bike] = {
-        for {
-          bike <- bikes.list
-        } yield Bike(bike._1, bike._2, bike._3)
-      }
+        val bikes = TableQuery[Bikes]
 
-      bikesList foreach { item =>
-        println("bike with id " + item.id + " has speed " + item.speed + "with description: "
-          + item.description)
-      }
-
-
-      def fetchBike(id: Long): Future[Option[Bike]] = Future {
-        bikesList.find(_.id == id)
-      }
-
-      def saveBike(bike: Bike): Future[Option[Done]] = Future {
-        bikesList.find(_.id == bike.id) match {
-          case Some(q) => None // Conflict! id is already taken
-          case None =>
-            bike :: bikesList // just saved locally!
-            Some(Done)
+        val bikesList: List[Bike] = {
+          for {
+            bike <- bikes.list
+          } yield Bike(bike._1, bike._2, bike._3)
         }
-      }
+
+        bikesList foreach { item =>
+          println("bike with id " + item.id + " has speed " + item.speed + "with description: "
+            + item.description)
+        }
 
 
-        implicit val system = ActorSystem()
-        implicit val executor = system.dispatcher
-        implicit val materializer = ActorMaterializer()
+        def fetchBike(id: Long): Option[Bike] = {
+          bikesList.find(_.id == id)
+        }
 
-        val config = ConfigFactory.load()
-        val logger = Logging(system, getClass)
+        def saveBike(bike: Bike): Option[Done] =  {
+          bikesList.find(_.id == bike.id) match {
+            case Some(q) => None // Conflict! id is already taken
+            case None =>
+              bike :: bikesList // just saved locally!
+              Some(Done)
+          }
+        }
+
 
         val route: Route =
           get {
             pathPrefix("bike" / LongNumber) { id =>
-              val maybeBike: Future[Option[Bike]] = fetchBike(id)
-
-              onSuccess(maybeBike) {
-                case Some(item) => complete(item)
-                case None => complete(StatusCodes.NotFound)
+              val maybeBike: Option[Bike] = fetchBike(id)
+              complete {
+                Some(maybeBike)
               }
             }
           } ~
             post {
               path("create-bike") {
                 entity(as[Bike]) { bike =>
-                  val saved: Future[Option[Done]] = saveBike(bike)
-                  onComplete(saved) { done =>
-                    complete("bike saved")
-
+                  val saved: Option[Done] = saveBike(bike)
+                  complete {
+                    Some(Done)
                   }
                 }
               }
 
             }
+
 
         val bindingFuture = Http().bindAndHandle(route, config.getString("http.interface"), config.getInt("http.port"))
 
@@ -94,6 +101,8 @@ class ShareMyBikeService(implicit val executionContext: ExecutionContext) {
         bindingFuture
           .flatMap(_.unbind())
           .onComplete(_ => system.terminate())
-      }
+
+
+    }
   }
 }
